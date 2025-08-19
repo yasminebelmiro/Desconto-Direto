d3531d73d2fc7c7abb457a85873ef9b2c0cbf94f
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2025-08-19 20:42:58 -0300

    feat: add a seção sobre

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,15 @@
+import About from "../components/About.tsx";
 import Header from "../components/Header.tsx";
 import HeroSection from "../components/HeroSection.tsx";
+import Steps from "../components/Steps.tsx";
 
 const LandingPage = () => {
   return (
     <div>
       <Header />
       <HeroSection />
+      <Steps />
+      <About />
     </div>
   );
 };
