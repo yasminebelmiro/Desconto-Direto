7910b93bc7a85e2559bc9cbe7d372fcaca7fe6c8
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2025-08-22 08:12:49 -0300

    chore: trocando as mensagens de erro por toast

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -5,11 +5,12 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { UserLoginSchema, type LoginData } from "../../schemas/LoginSchema.ts";
 import Input from "./components/Input.tsx";
+import { toast, ToastContainer } from "react-toastify";
 
 const Login = () => {
   const location = useLocation();
-   const navigate = useNavigate();
-   const isMerchantArea = location.pathname.includes("area-comerciantes");
+  const navigate = useNavigate();
+  const isMerchantArea = location.pathname.includes("area-comerciantes");
   const {
     register,
     handleSubmit,
@@ -18,38 +19,56 @@ const Login = () => {
 
   const onSubmit = (data: LoginData) => {
     console.log(data);
-
+    toast.success("Login realizado com sucesso!");
   };
+
+const onError = (errors: any) => {
+  Object.values(errors).forEach((err: any) => {
+    if (err?.message) {
+      toast.error(err.message);
+    }
+  });
+};
   return (
     <div>
       <Header />
       <div className="flex flex-col w-full md:flex-row items-center justify-center md:px-10 md:py-25 ">
-        <div className="hidden md:flex flex-col items-center justify-center bg-dark-blue md:w-100 lg:w-120 h-120 rounded-3xl">
+        <div className="hidden md:flex flex-col items-center justify-center bg-dark-blue md:w-100 lg:w-120 h-100 rounded-3xl">
           <img className="w-10" src={logo} alt="Logo DD" />
           <h1 className="font-kaisei flex flex-col text-center md:text-xl text-white">
             Desconto <span className="text-dark-yellow">Direto</span>
           </h1>
           <button
-            onClick={() => {isMerchantArea ? navigate("/area-comerciantes/cadastro") : navigate("/area-consumidores/cadastro")}}
+            onClick={() => {
+              isMerchantArea
+                ? navigate("/area-comerciantes/cadastro")
+                : navigate("/area-consumidores/cadastro");
+            }}
             className="font-kaisei bg-dark-yellow hover:bg-dark-orange cursor-pointer mt-10 w-1/2 px-10 py-2 rounded-2xl text-white"
           >
             Cadastrar
           </button>
         </div>
-        <div className="relative md:ml-[-35px] flex flex-col items-center justify-center bg-dark-yellow text-white w-full md:w-100 lg:w-120 h-120 gap-10 px-4 py-10 md:rounded-3xl ">
+        <div className="relative md:ml-[-35px] flex flex-col items-center justify-center bg-dark-yellow text-white w-full md:w-100 lg:w-120 h-100 gap-10 px-4 py-10 md:rounded-3xl ">
           <h1 className="font-kaisei text-3xl">Login</h1>
           <form
-            onSubmit={handleSubmit(onSubmit)}
+            onSubmit={handleSubmit(onSubmit, onError)}
             className="flex flex-col items-center justify-center w-full gap-4"
           >
-            <Input type="text" placeholder="Email" {...register("email")} />
-            {errors.email && (
-              <p className="text-red-600">{errors.email?.message}</p>
-            )}
-            <Input type="text" placeholder="Senha" {...register("senha")} />
-            {errors.senha && (
-              <p className="text-red-600">{errors.senha?.message}</p>
-            )}
+            <Input
+              type="email"
+              placeholder="Email"
+              error={errors.email?.message}
+              {...register("email")}
+            />
+
+            <Input
+              type="password"
+              placeholder="Senha"
+              error={errors.password?.message}
+              {...register("password")}
+            />
+
             <button
               type="submit"
               className="font-kaisei bg-dark-blue w-1/2 px-10 py-2 rounded-2xl hover:bg-dark-orange cursor-pointer"
@@ -57,7 +76,11 @@ const Login = () => {
               Entrar
             </button>
             <button
-              onClick={() => {isMerchantArea ? navigate("/area-comerciantes/cadastro") : navigate("/area-consumidores/cadastro")}}
+              onClick={() => {
+                isMerchantArea
+                  ? navigate("/area-comerciantes/cadastro")
+                  : navigate("/area-consumidores/cadastro");
+              }}
               className="font-kaisei md:hidden bg-dark-blue w-1/2 px-10 py-2 rounded-2xl hover:bg-dark-orange cursor-pointer"
             >
               Cadastrar
--- src/pages/Login/components/Input.tsx
+++ src/pages/Login/components/Input.tsx
@@ -1,16 +1,22 @@
 import { forwardRef, type InputHTMLAttributes } from "react";
 
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  error?: string | undefined;
+}
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-  return (
-    <input
-      className="bg-white text-dark-yellow placeholder:text-dark-yellow w-full h-15 px-10 rounded-lg"
-      ref={ref}
-      {...props}
-    />
-  );
-});
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ error, ...props }, ref) => {
+    return (
+      <input
+        className={`bg-white text-dark-yellow placeholder:text-dark-yellow 
+          w-full h-15 px-10 rounded-lg outline-none ${
+          error ? "border-2 border-red-500 text-red-500" : ""
+        }`}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
 
 export default Input;
