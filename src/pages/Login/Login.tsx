6c710ba401fb88a6400087cdd484d06c8823049f
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2025-08-22 08:31:48 -0300

    style: ajeitando o estilo da tela de login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -22,18 +22,18 @@ const Login = () => {
     toast.success("Login realizado com sucesso!");
   };
 
-const onError = (errors: any) => {
-  Object.values(errors).forEach((err: any) => {
-    if (err?.message) {
-      toast.error(err.message);
-    }
-  });
-};
+  const onError = (errors: any) => {
+    Object.values(errors).forEach((err: any) => {
+      if (err?.message) {
+        toast.error(err.message);
+      }
+    });
+  };
   return (
     <div>
       <Header />
       <div className="flex flex-col w-full md:flex-row items-center justify-center md:px-10 md:py-25 ">
-        <div className="hidden md:flex flex-col items-center justify-center bg-dark-blue md:w-100 lg:w-120 h-100 rounded-3xl">
+        <div className="hidden md:flex flex-col items-center justify-center bg-dark-blue md:w-100 lg:w-120 h-100 lg:h-120 rounded-3xl">
           <img className="w-10" src={logo} alt="Logo DD" />
           <h1 className="font-kaisei flex flex-col text-center md:text-xl text-white">
             Desconto <span className="text-dark-yellow">Direto</span>
@@ -49,7 +49,8 @@ const onError = (errors: any) => {
             Cadastrar
           </button>
         </div>
-        <div className="relative md:ml-[-35px] flex flex-col items-center justify-center bg-dark-yellow text-white w-full md:w-100 lg:w-120 h-100 gap-10 px-4 py-10 md:rounded-3xl ">
+        <div className="relative md:ml-[-35px] flex flex-col items-center justify-center bg-dark-yellow text-white w-full md:w-100 
+        lg:w-120 h-100 lg:h-120 gap-5 px-4 py-10 md:rounded-3xl ">
           <h1 className="font-kaisei text-3xl">Login</h1>
           <form
             onSubmit={handleSubmit(onSubmit, onError)}
@@ -69,26 +70,27 @@ const onError = (errors: any) => {
               {...register("password")}
             />
 
+            <p className="underline hover:font-bold">Esqueci minha senha</p>
             <button
               type="submit"
               className="font-kaisei bg-dark-blue w-1/2 px-10 py-2 rounded-2xl hover:bg-dark-orange cursor-pointer"
             >
               Entrar
             </button>
-            <button
+          </form>
+          <p>
+            Não tenho uma conta.{" "}
+            <span
+              className="underline hover:font-bold"
               onClick={() => {
                 isMerchantArea
                   ? navigate("/area-comerciantes/cadastro")
                   : navigate("/area-consumidores/cadastro");
               }}
-              className="font-kaisei md:hidden bg-dark-blue w-1/2 px-10 py-2 rounded-2xl hover:bg-dark-orange cursor-pointer"
             >
               Cadastrar
-            </button>
-            <p className="hover:underline hover:font-bold">
-              Esqueci minha senha
-            </p>
-          </form>
+            </span>
+          </p>
         </div>
       </div>
     </div>
