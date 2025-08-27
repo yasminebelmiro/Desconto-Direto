0518be0b12ba6e5b6c2bf8cdc6a6ab050d0e607f
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2025-08-27 08:07:35 -0300

    feat: finalização do header interno consumidores

--- src/pages/Consumer/Home/components/Header.tsx
+++ src/pages/Consumer/Home/components/Header.tsx
@@ -1,43 +1,50 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { FaRegBell, FaRegHeart, FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import logo from "../../../../assets/logo.png";
 
 import { GiHamburgerMenu } from "react-icons/gi";
+import { IoSearch } from "react-icons/io5";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <>
-      <section className="bg-dark-blue flex items-center justify-between px-10 py-5">
+      <section className="bg-dark-blue flex items-center justify-between
+       px-10 py-5 lg:px-30">
         <img className="w-8" src={logo} alt="Logo DD" />
         <button
           className="text-white text-2xl"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
-          <GiHamburgerMenu className="text-2xl hover:text-dark-yellow" />
+          <GiHamburgerMenu className="md:hidden text-2xl hover:text-dark-yellow" />
         </button>
-        <menu className="hidden md:flex gap-5">
+        <menu className="text-dark-yellow text-2xl hidden md:flex items-center
+         justify-center gap-5">
           <Link to={""}>
-            <FaSearch />
+            <IoSearch size={22} />
           </Link>
           <Link to={""}>
-            <FaRegBell />
+            <FaRegBell size={20} />
           </Link>
           <Link to={""}>
-            <FaRegHeart />
+            <FaRegHeart size={20} />
           </Link>
         </menu>
       </section>
       {isMenuOpen && (
-        <menu className="bg-dark-blue font-inter text-white w-full h-auto flex flex-col justify-between items-start p-8 gap-5">
-          <Link className="font-inter flex gap-5 items-center w-full hover:text-dark-yellow" to={""}>
+        <menu className="bg-dark-blue font-inter text-white w-full h-auto 
+        flex flex-col justify-between items-start p-8 gap-5">
+          <Link className="font-inter flex gap-5 items-center w-full
+           hover:text-dark-yellow" to={""}>
             <FaSearch  /> Pesquisar
           </Link>
-          <Link className="font-inter flex gap-5 items-center w-full hover:text-dark-yellow" to={""}>
+          <Link className="font-inter flex gap-5 items-center w-full
+           hover:text-dark-yellow" to={""}>
             <FaRegBell  /> Notificações
           </Link>
-          <Link className="font-inter flex gap-5 items-center w-full hover:text-dark-yellow" to={""}>
+          <Link className="font-inter flex gap-5 items-center w-full
+           hover:text-dark-yellow" to={""}>
             <FaRegHeart  /> Favoritos
           </Link>
         </menu>
