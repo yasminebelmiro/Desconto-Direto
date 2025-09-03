cd96b06585af109fb8b50d36544e9447253c2671
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2025-09-03 08:24:09 -0300

    feat: add componente +  mock de comercio e usuario

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,8 @@ Modal.setAppElement("#root");
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <RouterProvider  future={{
+    v7_startTransition: true,
+  }} router={Router} />
   </React.StrictMode>
 )
---
+++ src/pages/Consumer/MerchantProfile.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MerchantProfile = () => {
+  return (
+    <div>MerchantProfile</div>
+  )
+}
+
+export default MerchantProfile
