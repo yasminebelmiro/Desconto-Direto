b865fd85667749242be0eb0415056697557f76d3
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2025-09-11 10:44:41 -0300

    feat: montando etapas de cadastro

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import Router from './routes/Router.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import Router from "./routes/Router.tsx";
 
 import Modal from "react-modal";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
 Modal.setAppElement("#root");
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider  future={{
-    v7_startTransition: true,
-  }} router={Router} />
+    <Provider store={store}>
+      <RouterProvider future={{ v7_startTransition: true }} router={Router} />
+    </Provider>
   </React.StrictMode>
-)
+);
