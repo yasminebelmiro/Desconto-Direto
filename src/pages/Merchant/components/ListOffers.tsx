d3a1a647e144ed62ecb9979b5b0d9371f976178b
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2025-10-07 21:10:27 -0300

    style: arrumando o formato das datas

--- src/pages/Merchant/components/ListOffers.tsx
+++ src/pages/Merchant/components/ListOffers.tsx
@@ -6,20 +6,13 @@ interface ListOffersProps {
 }
 
 const ListOffers = ({offers}: ListOffersProps) => {
-    const formatedPrice = (price: number) => {
-        return price.toFixed(2).replace('.', ',');  
-    }
 
   return (
     <div className="flex items-center justify-center gap-10 py-10 md:px-10 flex-wrap lg:grid grid-cols-4 lg:w-250 ">
       {offers.map((offer) => (
         <OfferCard
           key={offer.id}
-          imgProduct={offer.produto.fotoUrl}
-          name={offer.produto.nome}
-          expiration={offer.validade}
-          price={formatedPrice(offer.preco)}
-          likes={offer.likes}
+          {...offer}
         />
       ))}
     </div>
